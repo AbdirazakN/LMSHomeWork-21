bbf91a2091abb7fdbd81435be6dc5bfab1293e0a
AbdirazakN <abdirazak.nooruzbai@gmail.com>
2022-11-29 19:08:31 +0600

    Commit

--- src/Main.java
+++ src/Main.java
@@ -33,64 +33,65 @@ public class Main {
         //36: 9
 
         writerLetters();
+//        writerLetters2();
         readerLetters();
     }
 
     private static void readerLetters() throws IOException {
         FileReader fileReader = new FileReader("EnglishLetters.txt");
         Scanner scanner = new Scanner(fileReader);
-        while (scanner.hasNextLine()){
-                System.out.println(scanner.nextLine());
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
         }
         fileReader.close();
     }
 
-//    private static void writerLetters() throws IOException {
-//        FileWriter fileWriter = new FileWriter("EnglishLetters.txt");
-//        fileWriter.write("A a");
-//        fileWriter.write("B b");
-//        fileWriter.write("C c");
-//        fileWriter.write("D d");
-//        fileWriter.write("E e");
-//        fileWriter.write("F f");
-//        fileWriter.write("G g");
-//        fileWriter.write("H h");
-//        fileWriter.write("I i");
-//        fileWriter.write("J j");
-//        fileWriter.write("K k");
-//        fileWriter.write("L l");
-//        fileWriter.write("M m");
-//        fileWriter.write("N n");
-//        fileWriter.write("O o");
-//        fileWriter.write("P p");
-//        fileWriter.write("Q q");
-//        fileWriter.write("R r");
-//        fileWriter.write("S s");
-//        fileWriter.write("T t");
-//        fileWriter.write("U u");
-//        fileWriter.write("V v");
-//        fileWriter.write("W w");
-//        fileWriter.write("X x");
-//        fileWriter.write("Y y");
-//        fileWriter.write("Z z");
-//        fileWriter.write("0");
-//        fileWriter.write("1");
-//        fileWriter.write("2");
-//        fileWriter.write("3");
-//        fileWriter.write("4");
-//        fileWriter.write("5");
-//        fileWriter.write("6");
-//        fileWriter.write("7");
-//        fileWriter.write("8");
-//        fileWriter.write("9");
-//        fileWriter.close();
-//    }
-
     private static void writerLetters() throws IOException {
         FileWriter fileWriter = new FileWriter("EnglishLetters.txt");
-        for (String s : Arrays.asList("A a", "B b", "C c", "D d", "E e", "F f", "G g", "H h", "I i", "J j", "K k", "L l", "M m", "N n", "O o", "P p", "Q q", "R r", "S s", "T t", "U u", "V v", "W w", "X x", "Y y", "Z z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")) {
-            fileWriter.write(s+"\n");
-        }
+        fileWriter.write("A a\n");
+        fileWriter.write("B b\n");
+        fileWriter.write("C c\n");
+        fileWriter.write("D d\n");
+        fileWriter.write("E e\n");
+        fileWriter.write("F f\n");
+        fileWriter.write("G g\n");
+        fileWriter.write("H h\n");
+        fileWriter.write("I i\n");
+        fileWriter.write("J j\n");
+        fileWriter.write("K k\n");
+        fileWriter.write("L l\n");
+        fileWriter.write("M m\n");
+        fileWriter.write("N n\n");
+        fileWriter.write("O o\n");
+        fileWriter.write("P p\n");
+        fileWriter.write("Q q\n");
+        fileWriter.write("R r\n");
+        fileWriter.write("S s\n");
+        fileWriter.write("T t\n");
+        fileWriter.write("U u\n");
+        fileWriter.write("V v\n");
+        fileWriter.write("W w\n");
+        fileWriter.write("X x\n");
+        fileWriter.write("Y y\n");
+        fileWriter.write("Z z\n");
+        fileWriter.write("0\n");
+        fileWriter.write("1\n");
+        fileWriter.write("2\n");
+        fileWriter.write("3\n");
+        fileWriter.write("4\n");
+        fileWriter.write("5\n");
+        fileWriter.write("6\n");
+        fileWriter.write("7\n");
+        fileWriter.write("8\n");
+        fileWriter.write("9\n");
         fileWriter.close();
     }
+
+//    private static void writerLetters2() throws IOException {
+//        FileWriter fileWriter = new FileWriter("EnglishLetters.txt");
+//        for (String s : Arrays.asList("A a", "B b", "C c", "D d", "E e", "F f", "G g", "H h", "I i", "J j", "K k", "L l", "M m", "N n", "O o", "P p", "Q q", "R r", "S s", "T t", "U u", "V v", "W w", "X x", "Y y", "Z z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")) {
+//            fileWriter.write(s + "\n");
+//        }
+//        fileWriter.close();
+//    }
 }
